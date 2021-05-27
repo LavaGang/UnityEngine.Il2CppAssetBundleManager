672defbefd01455a0c53d560c5b4df8d87c1250b
Sinai <outlander.sinai@gmail.com>
2021-05-27 14:48:02 +1000

    Don't reference AssetBundleModule, implement AssetBundleRequests

---
+++ Il2CppAssetBundleRequest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine.Scripting;
+using UnityEngineInternal;
+
+namespace UnityEngine
+{
+    public class AssetBundleCreateRequest : AsyncOperation
+    {
+        public AssetBundleCreateRequest(IntPtr ptr) : base(ptr) { }
+
+        public Il2CppAssetBundle assetBundle
+        {
+            get
+            {
+
+            }
+        }
+    }
+
+    public class AssetBundleRequest : AsyncOperation
+    {
+        public AssetBundleRequest(IntPtr ptr) : base(ptr) { }
+
+        public UnityEngine.Object asset
+        {
+            get
+            {
+
+            }
+        }
+
+        public UnityEngine.Object[] allAssets
+        {
+            get
+            {
+
+            }
+        }
+    }
+}
